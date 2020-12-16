a84dd3a313cf9abfcc0cbca31c878e95c1d4db71
Bora Yalcin <b.yalcin@catawiki.nl>
2020-12-16 11:48:43 +0100

    second part not done

--- src/day16/index.ts
+++ src/day16/index.ts
@@ -1,10 +1,30 @@
-import { flatten } from "lodash";
+import { flatten, inRange } from "lodash";
 import {
   logger,
   parseLinesToArray,
   readFileContent,
   parseByEmptyLinesToArray
 } from "../utils";
+
+// readFileContent("day16/input.txt")
+//   .then(parseByEmptyLinesToArray)
+//   .then((a) => a.map((r) => r.split("\n")))
+//   .then((parsed) => {
+//     const rules = parsed[0].map(parseRule);
+//     const yourTicket = parsed[1][1].split(",").map(Number);
+//     const otherTickets = parsed[2]
+//       .slice(1)
+//       .map((r) => r.split(",").map(Number));
+
+//     return { rules, yourTicket, otherTickets };
+//   })
+//   .then(({ rules, yourTicket, otherTickets }) => {
+//     const validated = otherTickets.map((t) => validateTicket(rules, t));
+
+//     return flatten(validated).reduce((t, c) => t + c, 0);
+//   })
+
+//   .then(logger);
 
 readFileContent("day16/input.txt")
   .then(parseByEmptyLinesToArray)
@@ -19,12 +39,54 @@ readFileContent("day16/input.txt")
     return { rules, yourTicket, otherTickets };
   })
   .then(({ rules, yourTicket, otherTickets }) => {
-    const validated = otherTickets.map((t) => validateTicket(rules, t));
+    const validated = otherTickets.filter(
+      (t) => validateTicket(rules, t).length === 0
+    );
+    console.log("otherTickets", otherTickets.length);
+    console.log("validated", validated.length);
 
-    return flatten(validated).reduce((t, c) => t + c, 0);
+    return { rules, yourTicket, otherTickets: validated };
   })
+  .then(({ rules, yourTicket, otherTickets }) => {
+    const validBag = runForList(rules, yourTicket, otherTickets);
+    console.log(validBag);
 
+    const result = validBag.reduce((t, k, i) => {
+      if (!k.startsWith("departure ")) {
+        return t;
+      }
+      console.log(yourTicket[i], t, i);
+      return yourTicket[i] * t;
+    }, 1);
+    console.log(result);
+  })
   .then(logger);
+
+function runForList(
+  rules: Rule[],
+  yourTicket: number[],
+  otherTickets: number[][],
+  validBag: string[] = []
+): string[] {
+  if (validBag.length === yourTicket.length) {
+    return validBag;
+  }
+
+  for (let i = 0; i < yourTicket.length; i++) {
+    const indexNumbers = otherTickets.map((a) => a[i]);
+    const allValid = rules.filter(
+      (r) =>
+        !validBag.includes(r[0]) &&
+        indexNumbers.every((n) => validateNumber(r[1], n))
+    );
+    if (allValid.length === 1) {
+      console.log(allValid);
+      validBag.push(allValid[0][0]);
+      return runForList(rules, yourTicket, otherTickets, validBag);
+    }
+  }
+  return [];
+}
 
 type RuleSet = [number, number][];
 type Rule = [string, RuleSet];
@@ -43,12 +105,12 @@ function parseRule(rule: string): Rule {
 function validateTicket(rules: Rule[], ticket: Ticket) {
   return ticket.filter((n) => {
     return !rules.some((rule) => {
-      // console.log(n, rule[1], validateNumber(rule[1], n));
       return validateNumber(rule[1], n);
     });
   });
 }
+
 function validateNumber(ruleSet: RuleSet, n: number) {
   const [a, b] = ruleSet;
-  return (n >= a[0] && n <= a[1]) || (n >= b[0] && n <= b[1]);
+  return inRange(n, a[0], a[1] + 1) || inRange(n, b[0], b[1] + 1);
 }
